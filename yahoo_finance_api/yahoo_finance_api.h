41843f939cf50f5dbb622c802a72d58679ec6ffd
Desmond Chi <desmond.chi.g@gmail.com>
2024-04-27 14:27:34 -0700

    Add docstrings to `YahooFinanceAPI`.

--- yahoo_finance_api/yahoo_finance_api.h
+++ yahoo_finance_api/yahoo_finance_api.h
@@ -11,12 +11,60 @@ namespace YahooFinanceAPI {
 
 class YahooFinanceAPI {
  public:
+  /**
+   * @brief Constructor.
+   *
+   */
   YahooFinanceAPI();
-  YahooFinanceAPI(absl::string_view ticker, const absl::Time& time = absl::Now());
+
+  /**
+   * @brief Constructor.
+   *
+   * @param ticker Name of ticker symbol.
+   * @param time Time for retrieving ticker information.
+   */
+  YahooFinanceAPI(absl::string_view ticker,
+                  const absl::Time& time = absl::Now());
+
+  /**
+   * @brief Set ticker symbol.
+   *
+   *
+   * @param ticker Name of ticker symbol.
+   */
   void SetTicker(absl::string_view ticker);
+
+  /**
+   * @brief Get name of ticker symbol.
+   *
+   *
+   * @return Name of ticker symbol.
+   */
   std::string GetTicker() const;
+
+  /**
+   * @brief Set time for retrieving ticker information.
+   *
+   *
+   * @param time Time for retrieving ticker information.
+   */
   void SetTime(const absl::Time& time);
+
+  /**
+   * @brief Get time for retrieving ticker information.
+   *
+   *
+   * @return Time for retrieving ticker information.
+   */
   absl::Time GetTime() const;
+
+  /**
+   * @brief Queries Yahoo Finance for information about the given ticker symbol
+   * and time.
+   *
+   *
+   * @return CSV of stock information in string format.
+   */
   std::string Query() const;
 
  private:
